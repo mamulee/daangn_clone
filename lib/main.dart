72b4629e0abd527b5a49e87b1d5999ed421e2958
mamulee <ghdah7@gmail.com>
2023-12-06 00:38:00 +0900

    home page

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("home page")),
     );
   }
 }
