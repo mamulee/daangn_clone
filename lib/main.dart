17708f54393b05cb850e589257687168e0f5d238
mamulee <ghdah7@gmail.com>
2023-12-06 00:48:15 +0900

    rough layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -22,7 +23,61 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("home page")),
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0.5,
+        leading: Row(
+          children: [
+            SizedBox(width: 16),
+            Text(
+              '중앙동',
+              style: TextStyle(
+                color: Colors.black,
+                fontWeight: FontWeight.bold,
+                fontSize: 20,
+              ),
+            ),
+            Icon(
+              Icons.keyboard_arrow_down_rounded,
+              color: Colors.black,
+            ),
+          ],
+        ),
+        leadingWidth: 100, // overflow 방지
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(CupertinoIcons.search, color: Colors.black),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.menu_rounded, color: Colors.black),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(CupertinoIcons.bell, color: Colors.black),
+          ),
+        ],
+      ),
+      body: Row(
+        children: [
+          // 이미지 들어갈 자리
+          Column(
+            children: [
+              // 'M1 아이패드 프로 11형(3세대) 와이파이 128G 팝니다.'
+              // '봉천동 · 6분 전'
+              // '100만원'
+              Row(
+                children: [
+                  // 빈 칸
+                  // 하트 아이콘
+                  // '1'
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
